dcca6636a994630fd8074915f293b5991ecae351
Prajwal Bhattarai <prajwalbhattarai80@gmail.com>
2024-09-07 23:01:30 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+console.log("Hello world");
+console.log("I am learning javascript...")
+
+
+//printing numbers
+console.log(1);
+console.log(2);
+
+//exercise 1
+//printing name ,age and occupation on the console
+console.log("My name is : Prajwal Bhattarai");
+console.log("My age is "+22);
+console.log("I am an student....");
+
+let message="Hello world";
+console.log(message);
+
+//printing values after assigning them to a variable
+ message="hello";
+console.log(message);
+
+ message="Hello world";
+console.log(message);
+
+// declaring constant variables
+const FILE_SIZE_LIMIT=2000;
+const MAX_SPEED=300;
+
+
+// variable declaration and scope of let, var and const keywords
+if(true){
+    var mName="Prajwal";
+}
+console.log(mName);
+
+if(true)
+{
+    let myName="Prazwol";
+
+}
+console.log(myName);
+
+// # Excercise 2
+// Write a program with three variables, each with the following value:
+
+// The first variable contains your name
+// The second variable contains your age
+// The third variable contains your occupation
+
+let name="prajwal bhattarai";
+let age=22;
+let occupation="Student";
+console.log(name);
+console.log(age);
+console.log(occupation);
